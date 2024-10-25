00f98d57ba7aaecbf16afb19b9a6265db1cee3c8
sriram2883 <144900269+sriram2883@users.noreply.github.com>
2024-10-25 10:57:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,6 +46,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/confirmedtours" element={<ConfirmedTours />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
