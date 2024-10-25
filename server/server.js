5b9e82f978eb0cf774c7cec08525eab24b037b30
Sriram Elluru <ellurisriram27655@gmail.com>
2024-10-25 14:39:54 +0530

    added initital verification

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors =require('cors')
+const jwt = require('jsonwebtoken');
 const corsOptions={
     origin:'*',
     optionsSuccessStatus:200
@@ -13,6 +14,15 @@ const AdminRoutes = require("./routes/AdminRoutes");
 const UserRoutes = require("./routes/UserRoutes");
 const GuideRoutes = require("./routes/GuideRoutes");
 const LoginRoutes = require("./routes/LoginRoutes");
+app.post('/verify',(req,res)=>{
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+        if(err){
+            res.status(403).send("Invalid token");
+        }
+        res.status(200).send("User is authenticated");
+    })
+})
 app.use('/admin',AdminRoutes);
 app.use('/user',UserRoutes);
 app.use('/guide',GuideRoutes);
